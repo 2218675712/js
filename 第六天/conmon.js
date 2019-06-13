7b70b4884a8fdac23f28b47a46d084cf420b2216
2218675712@qq.com <222186Xiuluo>
2019-06-13 20:07:16 +0800

    字符串的方法

--- 第六天/conmon.js
+++ 第六天/conmon.js
@@ -5,21 +5,21 @@
  */
 function getDate(dt) {
     //获取年
-    var year=dt.getFullYear();
+    var year = dt.getFullYear();
     //获取月
-    var month=dt.getMonth()+1;
+    var month = dt.getMonth() + 1;
     //获取日
-    var day=dt.getDate();
+    var day = dt.getDate();
     //获取小时
-    var hour=dt.getHours();
+    var hour = dt.getHours();
     //获取分钟
-    var minutes=dt.getMinutes();
+    var minutes = dt.getMinutes();
     //获取秒
-    var seconds=dt.getSeconds();
-    month=month<10?"0"+month:month;
-    day=day<10?"0"+day:day;
-    hour=hour<10?"0"+hour:hour;
-    minutes=minutes<10?"0"+minutes:minutes;
-    seconds=seconds<10?"0"+seconds:seconds;
-    return year+"年"+month+"月"+day+"日 "+hour+":"+minutes+":"+seconds;
+    var seconds = dt.getSeconds();
+    month = month < 10 ? "0" + month : month;
+    day = day < 10 ? "0" + day : day;
+    hour = hour < 10 ? "0" + hour : hour;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return year + "年" + month + "月" + day + "日 " + hour + ":" + minutes + ":" + seconds;
 }
